60ae3f1c2a5e5bfa316c9143dde42ce0b8630313
hassaankhan235 <hassaankhan235@gmail.com>
2020-06-06 14:39:42 +0300

    final commit

--- calc.js
+++ calc.js
@@ -1,18 +1,36 @@
-screen = document.querySelector(".txt")
-screen.value = "0"
-number = document.querySelector('.numbers')
-operator = document.querySelector('.operators',ops)
-equal = document.querySelector('.equal')
-equal.addEventListener('click',ops)
-number.addEventListener('click',Num)
-operator.addEventListener('click',ops)
+var screen = document.querySelector(".txt")
+var number = document.querySelector('.numbers')
+var operator = document.querySelector('.operators',ops)
+var equal = document.querySelector('.equal')
+var controls = document.querySelector('.controls')
+screen.value = " "
+screen.disabled= true
+var ON = false
+
 usrInp = {
     num1 : undefined,
-    num2 : undefined,
     op   : undefined
 }
 
+function OnOff(e) {
+    if(e.target.value == 'ON') {
+    console.log("ON")
+    screen.value = '0'
+    usrInp.op = undefined
+    usrInp.num1 = undefined
+    screen.disabled = false
+    ON = true
+    }
+    else if(e.target.value == "OFF") {
+        screen.disabled = true
+        screen.value = ''
+        ON = false
+    }
+}
+
 function Num(e) {
+    if (ON == true)
+    {
     if(screen.value == 0 || op_flag == true){
         op_flag = false
         screen.value = e.target.value
@@ -21,8 +39,11 @@ function Num(e) {
         screen.value += e.target.value
     }
 }
+}
 
 function ops(e) {
+    if (ON == true)
+    {
     if(usrInp.num1 == undefined || usrInp.op == '=') {
         usrInp.num1 = screen.value
         usrInp.op = e.target.value
@@ -56,3 +77,10 @@ else {
     op_flag = true
     console.log("operator pressed")
 }
+}
+
+
+equal.addEventListener('click',ops)
+number.addEventListener('click',Num)
+operator.addEventListener('click',ops)
+controls.addEventListener('click',OnOff)
